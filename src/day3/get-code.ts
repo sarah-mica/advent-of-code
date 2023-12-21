6079587850c2a35c148dcb9f6e9f40a134291f48
Sarah Mica <sarah.mica@headspace.com>
2023-12-21 13:21:02 -0800

    part 2 day 3

--- src/day3/get-code.ts
+++ src/day3/get-code.ts
@@ -36,11 +36,9 @@ function isValidNumber(schematic: string[], row: number, col: number, length: nu
     const maxCol = Math.min(col+length+1,schematic[0].length);
 
     for (let rowIndex=startRow; rowIndex<=maxRow; rowIndex++) {
-        console.log(rowIndex);
         const row = schematic[rowIndex];
         for (let col=startCol; col<maxCol; col++) {
             const c = row[col];
-            console.log(`Checking isValid: ${c}`);
 
             // if it's not a number and not . it's a "symbol"
             if(isNaN(parseInt(c)) && c !== '.') {
@@ -60,14 +58,11 @@ function sumPartNumbers(schematic: string[]): number {
         const row = schematic[rowIndex];
         for (let colIndex=0; colIndex<schematic[0].length; colIndex++) {
             const c = row[colIndex];
-            console.log(`c: ${c}`);
             const digit = parseInt(c);
             if (!isNaN(digit)) {
                 currNumber += c;
             } else {
-                // make sure the current number is valid
                 if (currNumber.length > 0) {
-                    console.log(`checking if ${currNumber} is valid`);
                     if (isValidNumber(schematic, rowIndex, colIndex-currNumber.length, currNumber.length)) {
                         validNumbers.push(parseInt(currNumber));
                     }
@@ -76,7 +71,6 @@ function sumPartNumbers(schematic: string[]): number {
             }
         }
         if (currNumber.length > 0) {
-            console.log(`checking (end of row) if ${currNumber} is valid`);
             if (isValidNumber(schematic, rowIndex, schematic[0].length-currNumber.length, currNumber.length)) {
                 validNumbers.push(parseInt(currNumber));
             }
@@ -90,17 +84,84 @@ function sumPartNumbers(schematic: string[]): number {
     return total;
 }
 
+/**
+ * Uses Cantor's pairing for unique hashing of two values
+ * https://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
+ * @returns unique hashed integer
+ */
+function hashRowCol(a: number, b: number): number {
+    return (a + b) * (a + b + 1) / 2 + a;
+}
+
+function findGears(schematic: string[], row: number, col: number, length: number, knownGears: Object, currNumber: string): void {
+    const startRow = Math.max(row-1, 0);
+    const maxRow = Math.min(row+1, schematic.length-1);
+    const startCol = Math.max(col-1, 0);
+    const maxCol = Math.min(col+length+1,schematic[0].length);
+
+    for (let rowIndex=startRow; rowIndex<=maxRow; rowIndex++) {
+        const row = schematic[rowIndex];
+        for (let col=startCol; col<maxCol; col++) {
+            const c = row[col];
+
+            // see if there's an adjacent gear. Add it to the known gears list
+            if(c === '*') {
+                console.log(`Found a gear at row: ${rowIndex}, col: ${col}`);
+                const hashedCoordinates = hashRowCol(rowIndex, col);
+                (knownGears[hashedCoordinates] || (knownGears[hashedCoordinates] = [])).push(parseInt(currNumber));
+            }
+        }
+    }
+}
+
+function sumGearRatios(schematic: string[]): number {
+    // dictionary of hashed row/col coordinates --> list of numbers seen by
+    let knownGears = {};
+    let currNumber = "";
+
+    for (let rowIndex=0; rowIndex<schematic.length; rowIndex++) {
+        const row = schematic[rowIndex];
+        for (let colIndex=0; colIndex<schematic[0].length; colIndex++) {
+            const c = row[colIndex];
+            const digit = parseInt(c);
+            if (!isNaN(digit)) {
+                currNumber += c;
+            } else {
+                if (currNumber.length > 0) {
+                    findGears(schematic, rowIndex, colIndex-currNumber.length, currNumber.length, knownGears, currNumber);
+                    currNumber = "";
+                }
+            }
+        }
+        if (currNumber.length > 0) {
+            findGears(schematic, rowIndex, schematic[0].length-currNumber.length, currNumber.length, knownGears, currNumber);
+            currNumber = "";
+        }
+    }
+    let sum = 0;
+    for (const key of Object.keys(knownGears)) {
+        const listOfNums = knownGears[key];
+        if (listOfNums.length === 2) {
+            sum += listOfNums[0] * listOfNums[1];
+        }
+    }
+    return sum;
+}
 
 
 // MAIN
 const fs = require("fs");
 
 const testFilesLoc = process.cwd() + "/src/day3/test-input.txt";
+const testGearsLoc = process.cwd() + "/src/day3/test-gears.txt";
 const puzzleFileLoc = process.cwd() + "/src/day3/puzzle-input.txt";
 
-console.log(`Looking for file ${puzzleFileLoc}`);
-const puzzleFile = fs.readFileSync(puzzleFileLoc).toString();
+const fileToRun = puzzleFileLoc;
+
+console.log(`Looking for file ${fileToRun}`);
+const puzzleFile = fs.readFileSync(fileToRun).toString();
 const schematicLines = puzzleFile.split("\n");
 
-const total = sumPartNumbers(schematicLines);
+//const total = sumPartNumbers(schematicLines);
+const total = sumGearRatios(schematicLines);
 console.log(total);
